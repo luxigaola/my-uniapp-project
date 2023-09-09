32a1af742d83331a6b5acde2b7f17c76751cdd94
Cai <caishaohua001@outlook.com>
2023-09-10 00:04:12 +0800

    feat:配置eslint

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # my-uniapp-project
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    uni(),
-  ],
+    plugins: [
+        uni(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
 })
