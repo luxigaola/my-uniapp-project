95a45f30a2aa3951c95711a90ac890cc7b19c31e
Cai <caishaohua001@outlook.com>
2023-09-10 17:41:02 +0800

    chore：删除无用文件

--- cm.cmd
+++
@@ -1,5 +0,0 @@
-
-@REM 您选择的配置需要以下依赖项：
-
-@REM eslint-plugin-vue@latest eslint-config-standard@latest eslint@^8.0.1 eslint-plugin-import@^2.25.2 eslint-plugin-n@^15.0.0 || ^16.0.0  eslint-plugin-promise@^6.0.0
-@REM ？您想现在安装它们吗？ » 否/是
